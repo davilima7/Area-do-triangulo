936b75724011f7e4e32ebf2d3086c509a902c283
davilima7 <134730328+davilima7@users.noreply.github.com>
2023-12-15 14:00:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('openPopupBtn').addEventListener('click', function() {
+    var popup = window.open('popup.html', 'janela', 'width=400, height=500, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no');
+
+    popup.onload = function() {
+        popup.document.getElementById('calculateBtn').addEventListener('click', function() {
+            var nota1 = parseFloat(popup.document.getElementById('nota1').value);
+            var nota2 = parseFloat(popup.document.getElementById('nota2').value);
+
+            var soma = nota1 * nota2;
+            var media = soma / 2;
+
+            window.opener.document.getElementById('result').innerHTML = `
+                <p>Soma das Notas: ${soma}</p>
+                <p>Média das Notas: ${media.toFixed(2)}</p>
+            `;
+
+            popup.close();
+        });
+    };
+});
